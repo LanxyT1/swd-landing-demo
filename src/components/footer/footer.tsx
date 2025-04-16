72b3c4ba801a336d3cbdfe2dbbd8d1ea02cb5d98
Ruslan Rezanov <ruslan.rezanov@swdfactory.com>
2025-04-16 17:55:34 +0300

    Added some more code to footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -37,6 +37,8 @@ import styles from "@/styles/Footer.module.scss"
 // export default Footer;
 
 const Footer = () => {
+    const addressLink = "https://www.google.com/maps?q=1600+Turaides+st.+10,+Riga,+Latvia";
+
     return (
       <footer className={styles.footer}>
           <div className={styles.section}></div>
@@ -46,9 +48,17 @@ const Footer = () => {
           <div className={styles.section}>
               <div className={styles.contactInfo}>
                   <span>E-mail address</span>
-
                   <a href="mailto:info@swdfactory.com">
                       info@swdfactory.com
+                  </a>
+              </div>
+
+              <div className={styles.contactInfo}>
+                  <span>Office location</span>
+                  <a href={addressLink}
+                     target="_blank"
+                     rel="noopener noreferrer">
+                      Turaides st. 10, Riga, Latvia
                   </a>
               </div>
           </div>
