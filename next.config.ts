a0af2b5e39776baa312678242a01a886f41bfd3a
Ruslan Rezanov <ruslan.rezanov@swdfactory.com>
2025-04-16 21:50:59 +0300

    config change

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
     reactStrictMode: true,
     images: {
-        path: "/"
+        path: "/swd-landing-demo/"
     },
 };
 
