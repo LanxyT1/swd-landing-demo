6314a06e03305911e21eef5d7242aaa60bfb63ca
Ruslan Rezanov <ruslan.rezanov@swdfactory.com>
2025-04-16 18:11:00 +0300

    Added header

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,40 +1,7 @@
 // Components
-import {SwdFactoryIcon} from "@/assets/icons";
+// import {SwdFactoryIcon} from "@/assets/icons";
 // Styles
 import styles from "@/styles/Footer.module.scss"
-
-// const Footer = () => {
-    // const addressLink = "https://www.google.com/maps?q=1600+Turaides+st.+10,+Riga,+Latvia";
-    // return (
-    //     <footer className={styles.footer}>
-    //         <div className={styles.section}>
-    //             <SwdFactoryIcon className="light" />
-    //         </div>
-
-    //         <div className={styles.lineBreak}></div>
-
-    //         <div className={styles.section}>
-    //             <div className={styles.rowSection}>
-    //                 <div className={styles.contactInfo}>
-    //                     <span>E-mail address</span>
-    //                     <a href="mailto:info@swdfactory.com">info@swdfactory.com</a>
-    //                 </div>
-
-    //                 <div className={styles.contactInfo}>
-    //                     <span>Office location</span>
-    //                     <a href={addressLink}
-    //                        target="_blank"
-    //                        rel="noopener noreferrer">Turaides st. 10, Riga, Latvia</a>
-    //                 </div>
-    //             </div>
-
-    //             <span>© 2025 SWD Factory. All Rights Reserved.</span>
-    //         </div>
-    //     </footer>
-    // );
-// };
-
-// export default Footer;
 
 const Footer = () => {
     const addressLink = "https://www.google.com/maps?q=1600+Turaides+st.+10,+Riga,+Latvia";
@@ -42,7 +9,7 @@ const Footer = () => {
     return (
       <footer className={styles.footer}>
           <div className={styles.section}>
-              <SwdFactoryIcon className="light" />
+              {/*<SwdFactoryIcon className="light" />*/}
           </div>
 
           <div className={styles.lineBreak}></div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
 // Next
 import type { AppProps } from "next/app";
+import { Manrope } from 'next/font/google'
 // Components
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
 // Styles
 import "@/styles/global.scss";
 
+const manrope = Manrope({ subsets: ['latin']});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={manrope.className}>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </div>
   );
 }
