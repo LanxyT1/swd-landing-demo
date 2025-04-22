e5f156ec3dd0cda2086bc495a62f4ac1dc26a2c1
Ruslan Rezanov <ruslan.rezanov@swdfactory.com>
2025-04-22 19:06:56 +0300

    Fixed issue with path

--- src/data/partners/partner-list.ts
+++ src/data/partners/partner-list.ts
@@ -1,27 +1,31 @@
+// Config
+import config from "../../../next.config";
+
+const pathUrl = `${config.basePath}/flow.svg`
 export const partnerList = [
     {
         name: "SunPower",
-        logo: "/partners/sunpower-logo.png",
+        logo: `${pathUrl}/partners/sunpower-logo.png`,
         link: "https://www.sunpowerglobal.com/int/"
     },
     {
         name: "Nanostring",
-        logo: "/partners/nanostring-logo.png",
+        logo: `${pathUrl}/partners/nanostring-logo.png`,
         link: "https://www.nanostring.com/"
     },
     {
         name: "BMS",
-        logo: "/partners/celgene-logo.png",
+        logo: `${pathUrl}/partners/celgene-logo.png`,
         link: "https://www.bms.com/"
     },
     {
         name: "Seattle BioSoftware",
-        logo: "/partners/seattle-biosoftware-logo.png",
+        logo: `${pathUrl}/partners/seattle-biosoftware-logo.png`,
         link: "https://www.seattlebiosoftware.com/"
     },
     {
         name: "PVStream",
-        logo: "/partners/pvstream-logo.png",
+        logo: `${pathUrl}/partners/pvstream-logo.png`,
         link: "https://www.pvstream.com/"
     }
 ];
